bd613de6b3b38be2bfe7fd5f863b3deaca442837
Vixtor272 <132792147+Vixtor272@users.noreply.github.com>
2025-12-06 21:49:20 -0500

    Actualización App, docker compose y export

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+# ==============================================================================
 # API REST PARA MODELO DE TRADING
+# ==============================================================================
 import joblib
 import pandas as pd
 import uvicorn
@@ -6,10 +8,10 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import os
 
-# 1. Inicializamos App
+# 1. Inicializar App
 app = FastAPI(title="Trading Prediction API", version="1.0")
 
-# 2. Cargamos el Modelo
+# 2. Cargar Modelo
 MODEL_PATH = os.getenv("MODEL_PATH", "trading_model.pkl")
 
 try:
@@ -19,7 +21,7 @@ except Exception as e:
     print(f"Error cargando modelo: {e}")
     model = None
 
-# 3. Esquema de los datos (Inputs)
+# 3. Esquema de datos (Inputs)
 class MarketFeatures(BaseModel):
     volume_rel_prev: float
     return_prev: float
